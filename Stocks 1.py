bdce1cb32d89ca4c8e96cf9b7107bd65b3204c8c
gbedward123 <64324618+gbedward123@users.noreply.github.com>
2022-09-18 13:11:05 -0400

    Add files via upload

---
+++ Stocks 1.py
@@ -0,0 +1,9 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:\Users\gilli\OneDrive\Documents\Books\Stocks assignment.py ====
+Amount paid for stock: $ 80000.0
+Commission paid on the purchase: $ 2400.0
+Amount the stock sold for: $ 85500.0
+Commission paid on the sale: $ 2565.0
+Profit (or loss if negative): $ 535.0
