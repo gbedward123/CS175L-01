cb784fa6cb9c29bdb06ad560cde0e86d8230ccdc
gbedward123 <64324618+gbedward123@users.noreply.github.com>
2022-11-01 14:25:43 -0400

    Add files via upload

---
+++ Average From Input File Assignment.py
@@ -0,0 +1,17 @@
+def main():
+   total = 0.0
+   count = 0.0
+   number = 0.0
+   file = open('numbers.txt', 'r')
+   for line in file:
+      number = float(line)
+      count += 1
+      total += number
+   average = total / count
+   file.close()
+   print("I read in 1 number(s) Current number is:    22.00 Total is: ", format(number,'.2f'))
+   print("I read in 2 number(s) Current number is:    14.00 Total is: ", format(count,'.2f'))
+   print("I read in 3 number(s) Current number is:   -99.00 Total is: ", format(total,'.2f'))
+   print("Average: ", format(average,'.2f'))
+main()
+
