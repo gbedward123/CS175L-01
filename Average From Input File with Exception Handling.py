86b6a722444f9d185ef3e0e732e9212ef7b73db7
gbedward123 <64324618+gbedward123@users.noreply.github.com>
2022-11-04 10:27:49 -0400

    Add files via upload

--- Average From Input File with Exception Handling.py
+++ Average From Input File with Exception Handling.py
@@ -10,14 +10,12 @@ try:
       total += number
    average = total / count
    file.close()
-   print("I read in 1 number(s) Current number is:    22.00 Total is: ", format(number,'.2f'))
-   print("I read in 2 number(s) Current number is:    14.00 Total is: ", format(count,'.2f'))
-   print("I read in 3 number(s) Current number is:   -99.00 Total is: ", format(total,'.2f'))
+   print("I read in "+str(count)+" number(s) Current number is:    "+str(number)+" Total is: "+format(number,'.2f'))
    print("Average: ", format(average,'.2f'))
 except IOError:
-    print('An error occured trying to read the file.')
+   print('An error occured trying to read the file.')
 except ValueError:
-    print('Non-numeric data found in the file.')
+   print('Non-numeric data found in the file.')
 except:
-    print('An error occured.')
+   print('An error occured.')
 main()
